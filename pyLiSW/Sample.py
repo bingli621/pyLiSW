Corrected errors in the calculation of spin wave intensity
Fixed the missing sqrt in the normalization of eigenvectors. Fixed the missing imaginary parts in calculating S(q,w) components. Implemented Cholesky decomposition. Fixed the issue when bands are degenerate.

pyLiSW/Sample.py
@@ -91,7 +91,7 @@ class Sample(object):
         to_print = []
         for idx, Bond in enumerate(Bonds):
             if np.array(Bond.j).ndim:
-                j = Bond.j.tolist()
+                j = np.array(Bond.j).tolist()
             else:
                 j = Bond.j
             d = np.round(

pyLiSW/utils.py
@@ -2,6 +2,34 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
+def gamma_fnc(x):
+    """
+    Customize damping FWHM gamma as a function of energy
+    """
+    # ARCS Ei=75 meV
+    # fwhm = +3.3349e-07 * x**3 + 0.00011656 * x**2 - 0.030234 * x + 2.8092
+    # CNCS Ei = 3.32 meV
+    # fwhm = +0.00030354 * x**3 + 0.0039009 * x**2 - 0.040862 * x + 0.11303
+    # CNCS Ei = 1.55 meV
+    # fwhm = +0.00096336 * x**3 + 0.005865 * x**2 - 0.027999 * x + 0.038397
+    # ARCS Ei=50 meV
+    # fwhm = +9.1948e-07 * x**3 + 0.00020961 * x**2 - 0.03571 * x + 1.9454
+    # ARCS Ei=125 meV
+    # fwhm = +1.4245e-07 * x**3 + 8.586e-05 * x**2 - 0.036398 * x + 5.5345 + 5
+    # fwhm = +1.5427e-07 * x**3 + 9.2779e-05 * x**2 - 0.039016 * x + 5.7486
+    fwhm = 1 * np.ones_like(x)
+    return fwhm
+
+
+def gamma_fnc_50(x):
+    """
+    Customize damping FWHM gamma as a function of energy
+    """
+    # ARCS Ei=50 meV
+    fwhm = +9.1948e-07 * x**3 + 0.00020961 * x**2 - 0.03571 * x + 1.9454
+    return fwhm
+
+
 def rot_ang(theta, phi):
     """
     Rotate angle theta about x axis and phi about z axis, in degrees
@@ -23,57 +51,83 @@ def rot_vec(theta, n):
     return np.array(r_mat)
 
 
-def gamma_fnc(x):
-    """
-    Customize damping FWHM gamma as a function of energy
-    """
-    # ARCS Ei=75 meV
-    # fwhm = +3.3349e-07 * x**3 + 0.00011656 * x**2 - 0.030234 * x + 2.8092
-    # CNCS Ei = 3.32 meV
-    # fwhm = +0.00030354 * x**3 + 0.0039009 * x**2 - 0.040862 * x + 0.11303
-    # CNCS Ei = 1.55 meV
-    # fwhm = +0.00096336 * x**3 + 0.005865 * x**2 - 0.027999 * x + 0.038397
-    # ARCS Ei=50 meV
-    fwhm = +9.1948e-07 * x**3 + 0.00020961 * x**2 - 0.03571 * x + 1.9454
-    # ARCS Ei=125 meV
-    # fwhm = +1.4245e-07 * x**3 + 8.586e-05 * x**2 - 0.036398 * x + 5.5345
-    fwhm = 1 * np.ones_like(x)
-    return fwhm
-
-
-import numpy as np
-
-
-def fold_slice(slice, err):
+def fold_slice(slice, err=None):
     """
     fold slice along x
     """
     amp = slice
     amp2 = np.flip(slice, axis=1)
-    err_sq = err**2
-    err_sq2 = np.flip(err, axis=1) ** 2
     cnt = np.nansum([~np.isnan(amp), ~np.isnan(amp2)], axis=0)
     # avoid warning of true_division
     np.seterr(divide="ignore", invalid="ignore")
     slice = np.nansum([amp, amp2], axis=0) / cnt
-    err = np.sqrt(np.nansum([err_sq, err_sq2], axis=0)) / cnt
     np.seterr(divide="warn", invalid="warn")
-    return slice, err
+    if err is None:
+        return slice
+    else:
+        err_sq = err**2
+        err_sq2 = np.flip(err, axis=1) ** 2
+        np.seterr(divide="ignore", invalid="ignore")
+        err = np.sqrt(np.nansum([err_sq, err_sq2], axis=0)) / cnt
+        np.seterr(divide="warn", invalid="warn")
+        return slice, err
 
 
-def add_data(data_list):
-    dim = np.shape(data_list[0])
+def add_intensity(intensity_list):
+    """
+    Avergae intesity of multiple slices of data or simulation
+    """
+    dim = np.shape(intensity_list[0])
     cnt_list = []
-    for data_set in data_list[0:]:
+    for data_set in intensity_list[0:]:
         if not np.shape(data_set) == dim:
             print("Dimensionality not matching.")
         else:
             cnt_list.append((~np.isnan(data_set)).astype(int))
 
-    data_sum = np.nansum(data_list, axis=0)
+    data_sum = np.nansum(intensity_list, axis=0)
     cnt_sum = np.nansum(cnt_list, axis=0)
     np.seterr(divide="ignore", invalid="ignore")
     data_avg = data_sum / cnt_sum
     np.seterr(divide="warn", invalid="warn")
 
     return data_avg
+
+
+def add_error(error_list):
+    """
+    Avergae error bars of multiple slices
+    """
+    dim = np.shape(error_list[0])
+    cnt_list = []
+    for err_set in error_list[0:]:
+        if not np.shape(err_set) == dim:
+            print("Dimensionality not matching.")
+        else:
+            cnt_list.append((~np.isnan(err_set)).astype(int))
+
+    err2_sum = np.sqrt(np.nansum(np.power(error_list, 2), axis=0))
+    cnt_sum = np.nansum(cnt_list, axis=0)
+    np.seterr(divide="ignore", invalid="ignore")
+    err_avg = err2_sum / cnt_sum
+    np.seterr(divide="warn", invalid="warn")
+
+    return err_avg
+
+
+def gauss_const(x, a, c, f, const):
+    """
+    Gaussian + constant.
+    a for amplitute, c for center, f for FWHM
+    """
+    s = f / (2 * np.sqrt(2 * np.log(2)))
+    return np.exp(-((x - c) ** 2) / (2 * s**2)) * a / (s * np.sqrt(2 * np.pi)) + const
+
+
+def gauss(x, a, c, f):
+    """
+    Gaussian + constant.
+    a for amplitute, c for center, f for FWHM
+    """
+    s = f / (2 * np.sqrt(2 * np.log(2)))
+    return np.exp(-((x - c) ** 2) / (2 * s**2)) * a / (s * np.sqrt(2 * np.pi))

tutorials/4_AFM_1d_Neel_supercell.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     n = (0, 1, 0)
     # temperature
     te = 20
-    afm_chain = Sample((a_eff, b_eff, c_eff), tau, n, te)
+    afm_chain = Sample((a_eff, b_eff, c_eff), tau, n, te, gamma_fnc=gamma_fnc)
     # -------------------------------------------------------------
     # Add atoms
     # -------------------------------------------------------------
@@ -37,7 +37,9 @@ if __name__ == "__main__":
     # atom postions with effective lattice parameters
     atoms = [
         Atoms(t=(0, 0, 0), ion="Mn2", spin=s1, aniso=aniso),
-        Atoms(t=(0.5, 0, 0), ion="Mn2", spin=s2, aniso=aniso, theta=np.pi),
+        Atoms(
+            t=(0.5, 0, 0), ion="Mn2", spin=s2, aniso=aniso, theta=np.pi, n_Rp=(0, 1, 0)
+        ),
     ]
     afm_chain.add_atoms(atoms)
     # -------------------------------------------------------------
@@ -72,7 +74,7 @@ if __name__ == "__main__":
         [0.00, 0.01],
         [-6, 6.01, 0.01],
     )
-    sim_qespace.inten_calc()
+    sim_qespace.inten_calc(mask=None)
     sim_qespace.slice(slice_range, plot_axes=(0, 3), SIM=True, vmin=0, vmax=20)
     # -------------------------------------------------------------
     # Making cuts